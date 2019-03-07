57d6c36cd1744c763d587e4a822cfc2ff20523c5
ETEML17 <antony.morais@neuf.fr>
2019-03-07 16:34:33 +0100

    creation vue inscription et suppression de fichiers inutilisés
    Signed-off-by: ETEML17 <antony.morais@neuf.fr>

---
+++ e-reputation/e-reputation_client/src/components/Connexion.vue
@@ -0,0 +1,49 @@
+<template>
+
+    <div>
+        <Header></Header>
+        <hr>
+
+        <b-row>
+            <b-col>
+                <Gauche></Gauche>
+            </b-col>
+            <b-col>
+                <form method="POST">
+                    <div class="form-group row">
+                        <label for="login" class="col-sm-2 col-form-label">Login</label>
+                        <div class="col-sm-10">
+                            <input type="text" class="form-control-plaintext" id="login" placeholder="Login">
+                        </div>
+                    </div>
+
+                    <div class="form-group row">
+                        <label for="motDePasse" class="col-sm-2 col-form-label">Mot de passe</label>
+                        <div class="col-sm-10">
+                            <input type="password" class="form-control-plaintext" id="motDePasse" placeholder="">
+                        </div>
+                    </div>
+
+                    <button type="submit" class="btn btn-primary">Se connecter</button>
+                </form>
+            </b-col>
+        </b-row>
+    </div>
+
+</template>
+
+<script>
+
+    import Header from './Header.vue'
+    import Gauche from './Gauche'
+
+    export default {
+        name: 'Connexion',
+        components: {Header,
+                     Gauche}
+    }
+</script>
+
+<style scoped>
+
+</style>
---
+++ e-reputation/e-reputation_client/src/components/Gauche.vue
@@ -0,0 +1,33 @@
+
+<template>
+    <b-row>
+        <b-col>
+            <div class="color">
+                <p>"Bienvenue sur votre portail E-Réputation.
+                    Pour superviser votre e-réputation,
+                    pour superviser vos achats fournisseurs"</p>
+                <img src="../assets/fiducial2.png"/>
+            </div>
+        </b-col>
+    </b-row>
+
+</template>
+
+<script>
+    export default {
+        name: 'Gauche'
+    }
+</script>
+
+<style scoped>
+    p {
+        padding-top: 50px;
+        font-style: italic;
+    }
+    .color {
+        background: linear-gradient(#E1DEDE, #6B6B6B);
+        padding-bottom: 50px;
+
+    }
+
+</style>
