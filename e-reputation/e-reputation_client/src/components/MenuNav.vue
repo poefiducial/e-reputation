88bc5731c1b8eb0b265ff7e7205f494976b137d3
Alexandre-Thomas3 <alexandre.thomas3@epsi.fr>
2019-03-11 09:24:57 +0100

    Creation vue : veoprint et twitter Modification: fiducial2.png en reduced
    Signed-off-by: Alexandre-Thomas3 <alexandre.thomas3@epsi.fr>

---
+++ e-reputation/e-reputation_client/src/components/MenuNav.vue
@@ -0,0 +1,42 @@
+<template>
+    <div>
+        <b-nav>
+            <div id="container">
+                <div class="head_container">
+                    <p>Vos portails</p>
+                </div>
+                <!--<div id="btn_container">-->
+                    <button class="home_button" type="button">Home</button><br>
+                    <button class="social_network_button" type="button">Réseaux sociaux</button><br>
+                    <button class="e_commerce_button" type="button">E-Commerce</button><br>
+                    <button class="dashboard_button" type="button">Dashboard</button><br>
+                    <button class="settings_button" type="button">Configurations</button>
+                <!--</div>-->
+                <img class="fidulogo" src="../assets/fiducial2_reduced.png">
+            </div>
+
+        </b-nav>
+    </div>
+</template>
+
+<script>
+    export default {
+        name: "MenuNav"
+    }
+</script>
+
+<style scoped>
+    #container{
+        border: solid;
+        border-width: 1px;
+        border-color: black;
+        background: linear-gradient(#E1DEDE, #6B6B6B);
+
+        display: flex;
+        flex-direction: column;
+        justify-content: space-around;
+    }
+    .fidulogo{
+        margin-top: 300px;
+    }
+</style>
