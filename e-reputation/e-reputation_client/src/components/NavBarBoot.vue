0e910dff08bdf2486da18eb331b08a47cd001e64
Alexandre-Thomas3 <alexandre.thomas3@epsi.fr>
2019-03-14 08:21:42 +0100

    NavBarBoot : mise en place de liens internes NavBarBoot : Tentative d'optimisation de <style> pour preserver la taille de l'objet sur toutes les pages. Toujours en cours.
    Signed-off-by: Alexandre-Thomas3 <alexandre.thomas3@epsi.fr>

--- e-reputation/e-reputation_client/src/components/NavBarBoot.vue
+++ e-reputation/e-reputation_client/src/components/NavBarBoot.vue
@@ -4,11 +4,12 @@
             <h4>Vos portails</h4>
         </b-row>
         <b-row class="btn_container">
-            <button class="home_button" type="button">Home</button>
-            <button class="social_network_button" type="button">Réseaux sociaux</button>
-            <button class="e_commerce_button" type="button">E-Commerce</button>
-            <button class="dashboard_button" type="button">Dashboard</button>
+            <button class="home_button" @click="goHome" type="button">Home</button>
+            <button class="social_network_button" @click="goTwitter" type="button">Réseaux sociaux</button>
+            <button class="e_commerce_button" @click="goVeoprint" type="button">E-Commerce</button>
+            <button class="dashboard_button" @click="goDashboard" type="button">Dashboard</button>
             <button class="settings_button" type="button">Configurations</button>
+            <button class="disconnect_button" @click="disconnect" type="button">Se déconnecter</button>
         </b-row>
         <b-row class="logo_container">
             <img class="fidulogo" src="../assets/fiducial2_reduced.png">
@@ -18,7 +19,25 @@
 
 <script>
     export default {
-        name: "NavBarBoot"
+        name: "NavBarBoot",
+
+        methods: {
+            goHome: function (){
+                this.$router.replace('/home');
+            },
+            goTwitter: function(){
+                this.$router.replace('/twitter')
+            },
+            goVeoprint: function(){
+                this.$router.replace('/veoprint')
+            },
+            goDashboard: function(){
+                this.$router.replace('/');
+            },
+            disconnect: function(){
+                this.$router.replace('/logout');
+            }
+        }
     }
     //var hauteur = window.innerHeight;
 </script>
@@ -26,7 +45,6 @@
 <style scoped>
     html{
         margin:0;
-        height: 700px
     }
     head, body, template{
         margin:0;
@@ -40,13 +58,12 @@
         width: 75%;
         margin: auto;
     }
-
     #nav_container{
-
         border: solid;
         border-width: 1px;
         background: linear-gradient(#E1DEDE, #6B6B6B);
         height: 100vh;
+        width: 20%;
     }
     .head_container, .btn_container, .logo_container{
         display: flex;
